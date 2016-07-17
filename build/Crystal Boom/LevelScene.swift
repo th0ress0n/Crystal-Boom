277de68ba1d97ee613f7493d62fae41e9b3b1e57
Per Thoresson <per@thoresson.com.au>
2016-07-18 03:21:24 +0400

    Complete Menu and Level Scene basics

--- build/Crystal Boom/LevelScene.swift
+++ build/Crystal Boom/LevelScene.swift
@@ -6,8 +6,40 @@
 //  Copyright © 2016 SAMBAVIKING. All rights reserved.
 //
 
-import UIKit
+import SpriteKit
 
-class LevelScene: SKNode {
 
+class LevelScene: SKScene {
+    
+    var nextBtn: SKNode! = nil
+    
+    override func didMoveToView(view: SKView) {
+        nextBtn = SKSpriteNode(color: SKColor.redColor(), size: CGSize(width: 100, height: 44))
+        nextBtn.position = CGPoint(x:CGRectGetMidX(self.frame), y:CGRectGetMidY(self.frame));
+        self.addChild(nextBtn)
+    }
+    
+    override func touchesBegan(touches: Set<UITouch>, withEvent event: UIEvent?) {
+        /* Called when a touch begins */
+        for touch: AnyObject in touches {
+            // Get the location of the touch in this scene
+            let location = touch.locationInNode(self)
+            // Check if the location of the touch is within the button's bounds
+            if nextBtn.containsPoint(location) {
+                print("tapped!")
+                
+                let transition = SKTransition.fadeWithDuration(1.0)//SKTransition.revealWithDirection(.Down, duration: 1.0)
+                
+                let nextScene = GameScene(size: scene!.size)
+                nextScene.scaleMode = .AspectFill
+                
+                scene?.view?.presentScene(nextScene, transition: transition)
+            }
+        }
+    }
+    
+    
+    override func update(currentTime: CFTimeInterval) {
+        /* Called before each frame is rendered */
+    }
 }
--- build/Crystal Boom/MenuScene.swift
+++ build/Crystal Boom/MenuScene.swift
@@ -6,8 +6,48 @@
 //  Copyright © 2016 SAMBAVIKING. All rights reserved.
 //
 
-import UIKit
+import SpriteKit
 
-class MenuScene: SKNode {
 
+class MenuScene: SKScene {
+    
+    var playBtn: SKNode! = nil
+    
+    override func didMoveToView(view: SKView) {
+        
+        playBtn = SKSpriteNode(color: SKColor.redColor(), size: CGSize(width: 100, height: 44))
+        playBtn.name = "playBtn"
+        playBtn.position = CGPoint(x:CGRectGetMidX(self.frame), y:CGRectGetMidY(self.frame));
+        self.addChild(playBtn)
+    }
+    
+    func open() -> Void {
+        print("open")
+        let scene = LevelScene(fileNamed: "LevelScene")!
+        let transition = SKTransition.moveInWithDirection(.Right, duration: 1)
+        self.view?.presentScene(scene, transition: transition)
+    }
+    
+    override func touchesBegan(touches: Set<UITouch>, withEvent event: UIEvent?) {
+        super.touchesBegan(touches, withEvent: event)
+        
+        if let location = touches.first?.locationInNode(self) {
+            let touchedNode = nodeAtPoint(location)
+            
+            print("touchedNode.name ",touchedNode.name)
+            
+            if touchedNode.name == "playBtn" {
+                let transition = SKTransition.fadeWithDuration(1.0)//SKTransition.revealWithDirection(.Down, duration: 1.0)
+                
+                let nextScene = LevelScene(size: scene!.size)
+                nextScene.scaleMode = .AspectFill
+                
+                scene?.view?.presentScene(nextScene, transition: transition)
+            }
+        }
+    }
+    
+    override func update(currentTime: CFTimeInterval) {
+        /* Called before each frame is rendered */
+    }
 }
